113c58cacc8fcf2b568ee9a428c2396699fc3fba
David Espinal <despinal0425@gmail.com>
2022-01-13 10:26:06 -0500

    added the uuid to the server sider

--- server/todolist/models.py
+++ server/todolist/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
 class Todo(models.Model):
+  uuid = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True, editable=False)
   title = models.CharField(max_length=120)
   description = models.TextField()
   completed = models.BooleanField(default=False)
