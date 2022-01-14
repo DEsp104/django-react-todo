804e2e790a7b7c7a38476628b3e011f211871fa8
David Espinal <despinal0425@gmail.com>
2022-01-14 12:40:30 -0500

    id in model has change to have uuid4

--- server/todolist/models.py
+++ server/todolist/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 import uuid
 
-# Create your models here.
-
 class Todo(models.Model):
-  uuid = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True, editable=False)
-  title = models.CharField(max_length=120)
-  description = models.TextField()
-  completed = models.BooleanField(default=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
 
-  def _str_(self):
-    return self.title
+    def _str_(self):
+        return self.title
+        
--- server/todolist/views.py
+++ server/todolist/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .serializers import TodoSerializer
 from .models import Todo
 
@@ -8,3 +8,5 @@ from .models import Todo
 class TodoView(viewsets.ModelViewSet):
   serializer_class = TodoSerializer
   queryset = Todo.objects.all()
+
+ 
